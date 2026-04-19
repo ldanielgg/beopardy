103935d4c8a16a6235a505792175ee4fd3c1a0ee
Danny Gonzalez <ldanielgg@hotmail.com>
2026-04-18 21:01:26 -0400

    complete initial lobby page styling

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
+const step1 = document.getElementById('setup-step-1');
+
+step1.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        generateNameFields(); // call the function to generate fields
+    }
+});
+
 function generateNameFields() {
     const count = document.getElementById('playerCount').value;
     const container = document.getElementById('name-fields-container');
-    const step1 = document.getElementById('setup-step-1');
     const step2 = document.getElementById('setup-step-2');
 
     // basic validation
     if (count < 1 || count > 4) {
         alert("Please select between 1 and 4 players.");
         return;
-    }
+    };
 
     // clear previous fields and generate new ones
-    container.innerHTML = `<h3>Enter Player Names</h3>`;
+    container.innerHTML = `<h3 class="input-label">Enter Player Names</h3>`;
     for (let i = 1; i <= count; i++) {
         container.innerHTML += `
             <div class="input-group">
-                <label>Player ${i}:</label>
-                <input type="text" name="player_names[]" placeholder="Enter Name" required>
+                <input type="text" name="player_names[]" placeholder="Player ${i}" class="input-field" required>
             </div>
         `;
-    }
+    };
 
     // transition UI
     step1.style.display = 'none';
     step2.style.display = 'flex';
-}
+};
